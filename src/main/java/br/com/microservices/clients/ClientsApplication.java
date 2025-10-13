b77f9722c05368df50030045b0fa380129888dfa
oRonold <or040918@gmail.com>
2025-10-13 15:45:39 -0300

    add register discovery client

--- src/main/java/br/com/microservices/clients/ClientsApplication.java
+++ src/main/java/br/com/microservices/clients/ClientsApplication.java
@@ -2,8 +2,10 @@ package br.com.microservices.clients;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ClientsApplication {
 
 	public static void main(String[] args) {
