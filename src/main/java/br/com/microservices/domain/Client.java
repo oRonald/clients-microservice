2ad0e9d8358aecee59264fd97e409260da4aa3af
oRonold <or040918@gmail.com>
2025-10-13 16:35:21 -0300

    create entity Client and repository

---
+++ src/main/java/br/com/microservices/domain/Client.java
@@ -0,0 +1,30 @@
+package br.com.microservices.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String cpf;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+    public Client(String cpf, String name, Integer age) {
+        this.cpf = cpf;
+        this.name = name;
+        this.age = age;
+    }
+}
---
+++ src/main/java/br/com/microservices/infrastructure/repository/ClientRepository.java
@@ -0,0 +1,11 @@
+package br.com.microservices.infrastructure.repository;
+
+import br.com.microservices.domain.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+
+    Optional<Client> findByCpf(String cpf);
+}
